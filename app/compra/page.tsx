55983eac94719fb4fde5d219be51206fe1adf596
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2024-02-27 14:22:50 -0300

    Update

---
+++ app/compra/page.tsx
@@ -0,0 +1,33 @@
+import { BackgroundGradientAnimation } from "@/app/gradient";
+import { SparklesCore } from "@/app/sparkles";
+
+export default function Compra(){
+    return(
+        <>
+        <BackgroundGradientAnimation
+      gradientBackgroundStart="rgb(217, 119, 6)"
+      gradientBackgroundEnd="rgb(234, 179, 8)"
+      firstColor="234, 179, 8"
+      secondColor="202, 138, 4"
+      thirdColor="rgb(251, 146, 60)"
+      fourthColor="rgb(252, 211, 77)"
+      interactive= {true}
+      >
+        <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+        background="transparent"
+        minSize={0.4}
+        maxSize={2}
+        particleDensity={120}
+        className="w-full h-full"
+        particleColor="#eab308"
+        />
+      </div>
+      <p>ss</p>
+      
+      
+
+      </BackgroundGradientAnimation>
+        </>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <>
     <Name />
-    <Button_Buy/>
     </>
     
   );

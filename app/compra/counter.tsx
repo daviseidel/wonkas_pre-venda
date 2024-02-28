04884ecae797a806f8ae7014a248d957b296fece
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2024-02-28 16:06:32 -0300

    .

--- app/compra/counter.tsx
+++ app/compra/counter.tsx
@@ -6,7 +6,7 @@ export default function Counter(){
     return(
 
 <form className="max-w-xs mx-auto">
-    <label htmlFor="bedrooms-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose quantity:</label>
+    <label htmlFor="bedrooms-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> </label>
     <div className="relative flex items-center max-w-[11rem]">
         <button onClick={() => setDoces(doces - 1)} type="button" id="decrement-button" data-input-counter-decrement="bedrooms-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
             <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
@@ -20,7 +20,6 @@ export default function Counter(){
             </svg>
         </button>
     </div>
-    <p id="helper-text-explanation" className=" text-sm text-gray-500 dark:text-gray-400">Please select the number of bedrooms.</p>
 </form>
 
     );
